cdcf51427b4e3bf7010a452b3ee99e4953d4ef17
Flavia Missi <flaviamissi@gmail.com>
2012-08-06 09:17:11 -0300

    keys: Suporting multiple key adition in authorized_keys

--- key.go
+++ key.go
@@ -1,18 +1,31 @@
 package gandalf
 
 import (
+    "fmt"
+    "io/ioutil"
 	"os"
+    "path"
 )
 
-var authKeys string = "authorized_keys"
+// file to write user's keys
+var authKey string = path.Join(os.Getenv("HOME"), "authorized_keys")
 
+// writes a key in authKey file
 func addKey(key string) error {
-	file, err := os.OpenFile(authKeys, os.O_WRONLY, 0755)
+	file, err := os.OpenFile(authKey, os.O_WRONLY, 0755)
 	defer file.Close()
 	if err != nil {
 		return err
 	}
-	_, err = file.Write([]byte(key))
+    keys, err := ioutil.ReadFile(authKey)
+	if err != nil {
+		return err
+	}
+    content := key
+    if len(keys) != 0 {
+        content = fmt.Sprintf("%s\n%s", keys, key)
+    }
+	_, err = file.Write([]byte(content))
 	if err != nil {
 		return err
 	}
--- key_test.go
+++ key_test.go
@@ -1,38 +1,56 @@
 package gandalf
 
 import (
+    "fmt"
+    "io/ioutil"
 	"os"
 	"testing"
+    "path"
 )
 
-func changeAuthKeys() {
-	authKeys = "testdata/authorized_keys"
+func changeAuthKey() {
+	authKey = "testdata/authorized_keys"
+}
+
+func clearAuthKeyFile() bool {
+    err := os.Truncate(authKey, 0)
+    if err != nil {
+        return false
+    }
+    return true
+}
+
+func TestAuthKeysShouldBeAbsolutePathToUsersAuthorizedKeys(t *testing.T) {
+    home := os.Getenv("HOME")
+    expected := path.Join(home, "authorized_keys")
+    if authKey != expected {
+        t.Errorf(`expected authKey to be %s, got: %s`, expected, authKey)
+    }
 }
 
 func TestShouldAddKeyWithoutError(t *testing.T) {
-	changeAuthKeys()
+	changeAuthKey()
 	key := "somekey blaaaaaaa r2d2@host"
 	err := addKey(key)
 	if err != nil {
 		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
 	}
+    ok := clearAuthKeyFile()
+    if !ok {
+        t.Errorf("Could not truncate file... :/")
+        t.FailNow()
+    }
 }
 
 func TestShouldWriteKeyInFile(t *testing.T) {
-	changeAuthKeys()
+	changeAuthKey()
 	key := "somekey blaaaaaaa r2d2@host"
 	err := addKey(key)
 	if err != nil {
 		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
 		t.FailNow()
 	}
-	file, err := os.Open(authKeys)
-	if err != nil {
-		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
-		t.FailNow()
-	}
-	b := make([]byte, len(key))
-	_, err = file.Read(b)
+	b, err := ioutil.ReadFile(authKey)
 	if err != nil {
 		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
 		t.FailNow()
@@ -41,4 +59,40 @@ func TestShouldWriteKeyInFile(t *testing.T) {
 	if got != key {
 		t.Errorf(`Expecing authorized_keys to be "%s", got "%s"`, key, got)
 	}
+    ok := clearAuthKeyFile()
+    if !ok {
+        t.Errorf("Could not truncate file... :/")
+        t.FailNow()
+    }
+}
+
+func TestShouldAppendKeyInFile(t *testing.T) {
+	changeAuthKey()
+	key1 := "somekey blaaaaaaa r2d2@host"
+	err := addKey(key1)
+	if err != nil {
+		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
+		t.FailNow()
+	}
+    key2 := "someotherkey fooo r2d2@host"
+	err = addKey(key2)
+	if err != nil {
+		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
+		t.FailNow()
+	}
+    b, err := ioutil.ReadFile(authKey)
+	if err != nil {
+		t.Errorf(`Expecting err to be nil, got: %s`, err.Error())
+		t.FailNow()
+	}
+	got := string(b)
+    expected := fmt.Sprintf("%s\n%s", key1, key2)
+	if got != expected {
+		t.Errorf(`Expecing authorized_keys to be "%s", got "%s"`, expected, got)
+	}
+    ok := clearAuthKeyFile()
+    if !ok {
+        t.Errorf("Could not truncate file... :/")
+        t.FailNow()
+    }
 }
