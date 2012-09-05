29bcb39d680e2f56619be517c0682b0b5d51c977
Flavia Missi <flaviamissi@gmail.com>
2012-09-05 17:42:51 -0300

    repository: created New function and added some basic validations to it

--- repository/repository.go
+++ repository/repository.go
@@ -1,7 +1,30 @@
 package repository
 
+import (
+	"errors"
+	"github.com/timeredbull/gandalf/db"
+)
+
 type Repository struct {
 	Name     string `bson:"_id"`
 	Users    []string
 	IsPublic bool
+}
+
+func New(name string, users []string, isPublic bool) (r *Repository, err error) {
+	// #TODO (flaviamissi) ensure repository name is a valid directory name
+	r = &Repository{Name: name, Users: users, IsPublic: isPublic}
+	if !r.isValid() {
+		err = errors.New("Validation Error: repository needs a valid name")
+		return
+	}
+	err = db.Session.Repository().Insert(&r)
+	return
+}
+
+func (r *Repository) isValid() bool {
+	if r.Name == "" {
+		return false
+	}
+	return true
 }
--- repository/repository_test.go
+++ repository/repository_test.go
@@ -1,1 +1,58 @@
 package repository
+
+import (
+	"github.com/timeredbull/gandalf/db"
+	"labix.org/v2/mgo/bson"
+	"testing"
+)
+
+func TestNewShouldCreateANew(t *testing.T) {
+	r, err := New("myRepo", []string{"smeagol", "saruman"}, true)
+	defer db.Session.Repository().Remove(bson.M{"_id": "myRepo"})
+	if err != nil {
+		t.Errorf("Unexpected error while creating new repository: %s", err.Error())
+	}
+	if r.Name != "myRepo" {
+		t.Errorf(`Expected repository name to be "myRepo", got "%s"`, r.Name)
+	}
+	if r.Users == nil {
+		t.Errorf(`Expected repository users to be {"smeagol", "saruman"}, got "%s"`, r.Users)
+	}
+	if !r.IsPublic {
+		t.Errorf(`Expected repository to be public`)
+	}
+}
+
+func TestNewShouldRecordItOnDatabase(t *testing.T) {
+	r, err := New("someRepo", []string{"smeagol"}, true)
+	defer db.Session.Repository().Remove(bson.M{"_id": "someRepo"})
+	if err != nil {
+		t.Errorf("Unexpected error while creating new repository: %s", err.Error())
+	}
+	err = db.Session.Repository().Find(bson.M{"_id": "someRepo"}).One(&r)
+	if err != nil {
+		t.Errorf("Unexpected error while retrieving new repository: %s", err.Error())
+	}
+	if r.Name != "someRepo" || r.Users == nil || !r.IsPublic {
+		t.Errorf("Retrieved parameters from database doens't match")
+	}
+}
+
+func TestNewBreaksOnValidationError(t *testing.T) {
+	_, err := New("", []string{"smeagol"}, false)
+	if err == nil {
+		t.Errorf("Expecting an error, got nil")
+	}
+	expected := "Validation Error: repository needs a valid name"
+	got := err.Error()
+	if got != expected {
+		t.Errorf(`Expected error to be "%s", got "%s"`, expected, got)
+	}
+}
+
+func TestRepositoryIsValid(t *testing.T) {
+	r := Repository{Users: []string{"gollum"}, IsPublic: true}
+	if r.isValid() {
+		t.Errorf("Expecting repository not to be valid")
+	}
+}
