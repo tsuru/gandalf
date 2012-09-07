79d46549ddbd8baaa9ebab57861f1df6873f47a4
Flavia Missi <flaviamissi@gmail.com>
2012-09-06 21:12:39 -0300

    user: created new and validation method

--- user/user.go
+++ user/user.go
@@ -1,6 +1,31 @@
 package user
 
+import (
+    "errors"
+    "github.com/timeredbull/gandalf/db"
+    "regexp"
+)
+
 type User struct {
 	Name string `bson:"_id"`
 	Keys []string
+}
+
+func New(name string, keys []string) (u *User, err error) {
+    u = &User{Name: name, Keys: keys}
+    err = db.Session.User().Insert(&u)
+    return
+}
+
+func (u *User) isValid() (v bool, err error) {
+    v = true
+	m, e := regexp.Match(`\s|[^aA-zZ0-9\.@]|(^$)`, []byte(u.Name))
+    if e != nil {
+        panic(e)
+    }
+    if m {
+        v = false
+        err = errors.New("Validation Error: user name is not valid")
+    }
+    return
 }
--- user/user_test.go
+++ user/user_test.go
@@ -1,1 +1,81 @@
 package user
+
+import (
+    "github.com/timeredbull/gandalf/db"
+    "testing"
+    "labix.org/v2/mgo/bson"
+)
+
+func TestNewUserReturnsAStructFilled(t *testing.T) {
+    u, err := New("someuser", []string{"id_rsa someKeyChars"})
+    if err != nil {
+        t.Errorf(`Got error while creating user: "%s"`, err.Error())
+    }
+    defer db.Session.User().Remove(bson.M{"_id": u.Name})
+    if u.Name != "someuser" {
+        t.Errorf(`Expected user name to be "someuser", got "%s"`, u.Name)
+    }
+    if len(u.Keys) == 0 {
+        t.Errorf(`Expected user to have 1 key, got %d`, len(u.Keys))
+    }
+}
+
+func TestNewUserShouldStoreUserInDatabase(t *testing.T) {
+    u, err := New("someuser", []string{"id_rsa someKeyChars"})
+    if err != nil {
+        t.Errorf(`Got error while creating user: "%s"`, err.Error())
+    }
+    defer db.Session.User().Remove(bson.M{"_id": u.Name})
+    err = db.Session.User().Find(bson.M{"_id": u.Name}).One(&u)
+    if err != nil {
+        t.Errorf(`Got error while creating user: "%s"`, err.Error())
+    }
+    if u.Name != "someuser" {
+        t.Errorf(`Expected user name to be "someuser", got "%s"`, u.Name)
+    }
+    if len(u.Keys) == 0 {
+        t.Errorf(`Expected user to have 1 key, got %d`, len(u.Keys))
+    }
+}
+
+func TestIsValidReturnsErrorWhenUserDoesNotHaveAName(t *testing.T) {
+    u := User{Keys: []string{"id_rsa foooBar"}}
+    v, err := u.isValid()
+    if v {
+        t.Errorf(`Expected user to be invalid`)
+    }
+    if err == nil {
+        t.Errorf(`Expected error not to be nil`)
+        t.FailNow()
+    }
+    expected := "Validation Error: user name is not valid"
+    got := err.Error()
+    if got != expected {
+        t.Errorf(`Expected error to be "%s", got "%s"`, expected, got)
+    }
+}
+
+func TestIsValidShouldNotAcceptEmptyUserName(t *testing.T) {
+    u := User{Name: "", Keys: []string{"id_rsa foooBar"}}
+    v, err := u.isValid()
+    if v {
+        t.Errorf(`Expected user to be invalid`)
+    }
+    if err == nil {
+        t.Errorf(`Expected error not to be nil`)
+        t.FailNow()
+    }
+    expected := "Validation Error: user name is not valid"
+    got := err.Error()
+    if got != expected {
+        t.Errorf(`Expected error to be "%s", got "%s"`, expected, got)
+    }
+}
+
+func TestIsValidShouldAcceptEmailsAsUserName(t *testing.T) {
+    u := User{Name: "r2d2@gmail.com", Keys: []string{"id_rsa foooBar"}}
+    v, _ := u.isValid()
+    if !v {
+        t.Errorf(`Expected user to be valid`)
+    }
+}
