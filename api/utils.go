027eebea95aec7b4efa982bd042df83129d41f03
Flavia Missi <flaviamissi@gmail.com>
2012-11-09 18:01:45 -0200

    api: minor refactor in utils.go

--- api/utils.go
+++ api/utils.go
@@ -8,10 +8,9 @@ import (
 )
 
 func getUserOr404(name string) (user.User, error) {
-	u := user.User{}
-	err := db.Session.User().Find(bson.M{"_id": name}).One(&u)
-	if err != nil && err.Error() == "not found" {
-		err = fmt.Errorf("User %s not found", name)
+	var u user.User
+	if err := db.Session.User().Find(bson.M{"_id": name}).One(&u); err != nil && err.Error() == "not found" {
+		return u, fmt.Errorf("User %s not found", name)
 	}
-	return u, err
+	return u, nil
 }
