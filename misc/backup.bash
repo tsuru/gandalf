c4f6e8d51cad1d432985854fb2d5d67c56f600b4
Andrews Medina <andrewsmedina@gmail.com>
2013-01-14 18:13:40 -0200

    Using s3cmd put instead cp.

--- misc/backup.bash
+++ misc/backup.bash
@@ -5,10 +5,12 @@
 # license that can be found in the LICENSE file.
 
 # This script is used to backup on s3 repositories created by gandalf.
+#
+# To use this script it's need install and configure the s3cmd.
 
 function send_to_s3 {
     echo "Sending $1 to $2 in s3 ..."
-    s3cmd cp $1 $2
+    s3cmd put $1 $2
 }
 
 function compact {
@@ -19,4 +21,5 @@ function compact {
 # making the backup for authorized_keys
 [ -f "${HOME}/.ssh/authorized_keys" ]  && send_to_s3 "${HOME}/.ssh/authorized_keys" $1
 
+# making the backup for repositories files
 [ -d $2 ] && send_to_s3 compact $2 $1
