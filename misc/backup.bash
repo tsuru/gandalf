c75658a6df6818af31f18401a8982657fff7d5d3
Andrews Medina <andrewsmedina@gmail.com>
2013-01-15 08:55:42 -0200

    Refactored the backup.bash

--- misc/backup.bash
+++ misc/backup.bash
@@ -8,14 +8,16 @@
 #
 # To use this script it's need install and configure the s3cmd.
 
+name="repositories.tar.gz"
+
 function send_to_s3 {
     echo "Sending $1 to $2 in s3 ..."
     s3cmd put $1 $2
 }
 
 function compact {
-    echo "Compacting $1 into $2 ..."
-    tar zcvf $2 $1
+    echo "Compacting $1 into ${name} ..."
+    tar zcvf ${name} $1
 }
 
 # making the backup for authorized_keys
@@ -24,7 +26,6 @@ function compact {
 # making the backup for repositories files
 if [ -d $2 ]; then
     echo "making the backup for repositories files..."
-    name="repositories.tar.gz"
-    compact $2 $name
-    send_to_s3 $name $1
+    compact $2
+    send_to_s3 ${name} $1
 fi
