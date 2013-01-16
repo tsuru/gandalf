59b141d41047528b3eaca6e98f86579dd608692d
Francisco Souza <fss@corp.globo.com>
2013-01-16 14:54:45 -0200

    all: added license headers to files

--- fs/filesystem.go
+++ fs/filesystem.go
@@ -1,3 +1,7 @@
+// Copyright 2013 gandalf authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 // fs is just a filesystem wrapper.
 // It makes use of tsuru/fs pkg.
 package fs
