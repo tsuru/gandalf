e959a0079dd81655048734239c8a0cb3a9fa1223
Michael <hfeeki@gmail.com>
2013-02-09 18:53:00 +0800

    Update setup/install.sh
    update "git daemon" param

--- setup/install.sh
+++ setup/install.sh
@@ -10,4 +10,4 @@ sudo cp etc/gandalf.conf /etc/
 # starts gandalf api web server
 go build -o gandalf-webserver webserver/main.go
 ./gandalf-webserver > $HOME/gandalf-webserver.out 2>&1 &
-git daemon --base-path /var/repositories --syslog --export-all
+git daemon --base-path=/var/repositories --syslog --export-all
