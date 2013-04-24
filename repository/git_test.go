100f5c78520e6d8fb03fb38b54e7b84ac516ae6b
Francisco Souza <fss@corp.globo.com>
2013-04-24 12:50:51 -0300

    repository: don't use unqualified imports for gocheck

--- repository/git_test.go
+++ repository/git_test.go
@@ -10,93 +10,93 @@ import (
 	"github.com/globocom/config"
 	"github.com/globocom/gandalf/fs"
 	"github.com/globocom/tsuru/fs/testing"
-	. "launchpad.net/gocheck"
+	"launchpad.net/gocheck"
 	"path"
 )
 
-func (s *S) TestBareLocationValuShouldComeFromGandalfConf(c *C) {
+func (s *S) TestBareLocationValuShouldComeFromGandalfConf(c *gocheck.C) {
 	bare = ""
 	config.Set("git:bare:location", "/home/gandalf")
 	l := bareLocation()
-	c.Assert(l, Equals, "/home/gandalf")
+	c.Assert(l, gocheck.Equals, "/home/gandalf")
 }
 
-func (s *S) TestBareLocationShouldResetBareValue(c *C) {
+func (s *S) TestBareLocationShouldResetBareValue(c *gocheck.C) {
 	l := bareLocation()
 	config.Set("git:bare:location", "fooo/baaar")
-	c.Assert(bareLocation(), Equals, l)
+	c.Assert(bareLocation(), gocheck.Equals, l)
 }
 
-func (s *S) TestNewBareShouldCreateADir(c *C) {
+func (s *S) TestNewBareShouldCreateADir(c *gocheck.C) {
 	dir, err := commandmocker.Add("git", "$*")
-	c.Check(err, IsNil)
+	c.Check(err, gocheck.IsNil)
 	defer commandmocker.Remove(dir)
 	err = newBare("myBare")
-	c.Assert(err, IsNil)
-	c.Assert(commandmocker.Ran(dir), Equals, true)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(commandmocker.Ran(dir), gocheck.Equals, true)
 }
 
-func (s *S) TestNewBareShouldReturnMeaningfullErrorWhenBareCreationFails(c *C) {
+func (s *S) TestNewBareShouldReturnMeaningfullErrorWhenBareCreationFails(c *gocheck.C) {
 	dir, err := commandmocker.Error("git", "ooooi", 1)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(dir)
 	err = newBare("foo")
-	c.Check(err, NotNil)
+	c.Check(err, gocheck.NotNil)
 	got := err.Error()
 	expected := "Could not create git bare repository: exit status 1"
-	c.Assert(got, Equals, expected)
+	c.Assert(got, gocheck.Equals, expected)
 }
 
-func (s *S) TestNewBareShouldPassTemplateOptionWhenItExistsOnConfig(c *C) {
+func (s *S) TestNewBareShouldPassTemplateOptionWhenItExistsOnConfig(c *gocheck.C) {
 	bareTemplate := "/var/templates"
 	bareLocation, err := config.GetString("git:bare:location")
 	config.Set("git:bare:template", bareTemplate)
 	defer config.Unset("git:bare:template")
 	barePath := path.Join(bareLocation, "foo.git")
 	dir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(dir)
 	err = newBare("foo")
-	c.Assert(err, IsNil)
-	c.Assert(commandmocker.Ran(dir), Equals, true)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(commandmocker.Ran(dir), gocheck.Equals, true)
 	expected := fmt.Sprintf("init %s --bare --template=%s", barePath, bareTemplate)
-	c.Assert(commandmocker.Output(dir), Equals, expected)
+	c.Assert(commandmocker.Output(dir), gocheck.Equals, expected)
 }
 
-func (s *S) TestNewBareShouldNotPassTemplateOptionWhenItsNotSetInConfig(c *C) {
+func (s *S) TestNewBareShouldNotPassTemplateOptionWhenItsNotSetInConfig(c *gocheck.C) {
 	config.Unset("git:bare:template")
 	bareLocation, err := config.GetString("git:bare:location")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	barePath := path.Join(bareLocation, "foo.git")
 	dir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(dir)
 	err = newBare("foo")
-	c.Assert(err, IsNil)
-	c.Assert(commandmocker.Ran(dir), Equals, true)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(commandmocker.Ran(dir), gocheck.Equals, true)
 	expected := fmt.Sprintf("init %s --bare", barePath)
-	c.Assert(commandmocker.Output(dir), Equals, expected)
+	c.Assert(commandmocker.Output(dir), gocheck.Equals, expected)
 }
 
-func (s *S) TestRemoveBareShouldRemoveBareDirFromFileSystem(c *C) {
+func (s *S) TestRemoveBareShouldRemoveBareDirFromFileSystem(c *gocheck.C) {
 	rfs := &testing.RecordingFs{FileContent: "foo"}
 	fs.Fsystem = rfs
 	defer func() { fs.Fsystem = nil }()
 	err := removeBare("myBare")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	action := "removeall " + path.Join(bareLocation(), "myBare.git")
-	c.Assert(rfs.HasAction(action), Equals, true)
+	c.Assert(rfs.HasAction(action), gocheck.Equals, true)
 }
 
-func (s *S) TestRemoveBareShouldReturnDescriptiveErrorWhenRemovalFails(c *C) {
+func (s *S) TestRemoveBareShouldReturnDescriptiveErrorWhenRemovalFails(c *gocheck.C) {
 	rfs := &testing.RecordingFs{FileContent: "foo"}
 	fs.Fsystem = &testing.FailureFs{RecordingFs: *rfs}
 	defer func() { fs.Fsystem = nil }()
 	err := removeBare("fooo")
-	c.Assert(err, ErrorMatches, "^Could not remove git bare repository: .*")
+	c.Assert(err, gocheck.ErrorMatches, "^Could not remove git bare repository: .*")
 }
 
-func (s *S) TestFormatNameShouldAppendDotGitInTheEndOfTheRepoName(c *C) {
+func (s *S) TestFormatNameShouldAppendDotGitInTheEndOfTheRepoName(c *gocheck.C) {
 	rName := formatName("myrepo")
-	c.Assert(rName, Equals, "myrepo.git")
+	c.Assert(rName, gocheck.Equals, "myrepo.git")
 }
--- repository/repository_test.go
+++ repository/repository_test.go
@@ -13,284 +13,284 @@ import (
 	"github.com/globocom/gandalf/fs"
 	fstesting "github.com/globocom/tsuru/fs/testing"
 	"labix.org/v2/mgo/bson"
-	. "launchpad.net/gocheck"
+	"launchpad.net/gocheck"
 	"path"
 	"testing"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func Test(t *testing.T) { gocheck.TestingT(t) }
 
 type S struct {
 	tmpdir string
 }
 
-var _ = Suite(&S{})
+var _ = gocheck.Suite(&S{})
 
-func (s *S) SetUpSuite(c *C) {
+func (s *S) SetUpSuite(c *gocheck.C) {
 	err := config.ReadConfigFile("../etc/gandalf.conf")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	config.Set("database:url", "127.0.0.1:27017")
 	config.Set("database:name", "gandalf_repository_tests")
 	db.Connect()
 }
 
-func (s *S) TearDownSuite(c *C) {
+func (s *S) TearDownSuite(c *gocheck.C) {
 	db.Session.DB.DropDatabase()
 }
 
-func (s *S) TestNewShouldCreateANewRepository(c *C) {
+func (s *S) TestNewShouldCreateANewRepository(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	users := []string{"smeagol", "saruman"}
 	r, err := New("myRepo", users, true)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().Remove(bson.M{"_id": "myRepo"})
-	c.Assert(r.Name, Equals, "myRepo")
-	c.Assert(r.Users, DeepEquals, users)
-	c.Assert(r.IsPublic, Equals, true)
+	c.Assert(r.Name, gocheck.Equals, "myRepo")
+	c.Assert(r.Users, gocheck.DeepEquals, users)
+	c.Assert(r.IsPublic, gocheck.Equals, true)
 }
 
-func (s *S) TestNewShouldRecordItOnDatabase(c *C) {
+func (s *S) TestNewShouldRecordItOnDatabase(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	r, err := New("someRepo", []string{"smeagol"}, true)
 	defer db.Session.Repository().Remove(bson.M{"_id": "someRepo"})
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().Find(bson.M{"_id": "someRepo"}).One(&r)
-	c.Assert(err, IsNil)
-	c.Assert(r.Name, Equals, "someRepo")
-	c.Assert(r.Users, DeepEquals, []string{"smeagol"})
-	c.Assert(r.IsPublic, Equals, true)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(r.Name, gocheck.Equals, "someRepo")
+	c.Assert(r.Users, gocheck.DeepEquals, []string{"smeagol"})
+	c.Assert(r.IsPublic, gocheck.Equals, true)
 }
 
-func (s *S) TestNewBreaksOnValidationError(c *C) {
+func (s *S) TestNewBreaksOnValidationError(c *gocheck.C) {
 	_, err := New("", []string{"smeagol"}, false)
-	c.Check(err, NotNil)
+	c.Check(err, gocheck.NotNil)
 	expected := "Validation Error: repository name is not valid"
 	got := err.Error()
-	c.Assert(got, Equals, expected)
+	c.Assert(got, gocheck.Equals, expected)
 }
 
-func (s *S) TestRepositoryIsNotValidWithoutAName(c *C) {
+func (s *S) TestRepositoryIsNotValidWithoutAName(c *gocheck.C) {
 	r := Repository{Users: []string{"gollum"}, IsPublic: true}
 	v, err := r.isValid()
-	c.Assert(v, Equals, false)
-	c.Check(err, NotNil)
+	c.Assert(v, gocheck.Equals, false)
+	c.Check(err, gocheck.NotNil)
 	got := err.Error()
 	expected := "Validation Error: repository name is not valid"
-	c.Assert(got, Equals, expected)
+	c.Assert(got, gocheck.Equals, expected)
 }
 
-func (s *S) TestRepositoryIsNotValidWithInvalidName(c *C) {
+func (s *S) TestRepositoryIsNotValidWithInvalidName(c *gocheck.C) {
 	r := Repository{Name: "foo bar", Users: []string{"gollum"}, IsPublic: true}
 	v, err := r.isValid()
-	c.Assert(v, Equals, false)
-	c.Check(err, NotNil)
+	c.Assert(v, gocheck.Equals, false)
+	c.Check(err, gocheck.NotNil)
 	got := err.Error()
 	expected := "Validation Error: repository name is not valid"
-	c.Assert(got, Equals, expected)
+	c.Assert(got, gocheck.Equals, expected)
 }
 
-func (s *S) TestRepositoryShoudBeInvalidWIthoutAnyUsers(c *C) {
+func (s *S) TestRepositoryShoudBeInvalidWIthoutAnyUsers(c *gocheck.C) {
 	r := Repository{Name: "foo_bar", Users: []string{}, IsPublic: true}
 	v, err := r.isValid()
-	c.Assert(v, Equals, false)
-	c.Assert(err, NotNil)
+	c.Assert(v, gocheck.Equals, false)
+	c.Assert(err, gocheck.NotNil)
 	got := err.Error()
 	expected := "Validation Error: repository should have at least one user"
-	c.Assert(got, Equals, expected)
+	c.Assert(got, gocheck.Equals, expected)
 }
 
-func (s *S) TestRepositoryShouldBeValidWithoutIsPublic(c *C) {
+func (s *S) TestRepositoryShouldBeValidWithoutIsPublic(c *gocheck.C) {
 	r := Repository{Name: "someName", Users: []string{"smeagol"}}
 	v, _ := r.isValid()
-	c.Assert(v, Equals, true)
+	c.Assert(v, gocheck.Equals, true)
 }
 
-func (s *S) TestNewShouldCreateNewGitBareRepository(c *C) {
+func (s *S) TestNewShouldCreateNewGitBareRepository(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	_, err = New("myRepo", []string{"pumpkin"}, true)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().Remove(bson.M{"_id": "myRepo"})
-	c.Assert(commandmocker.Ran(tmpdir), Equals, true)
+	c.Assert(commandmocker.Ran(tmpdir), gocheck.Equals, true)
 }
 
-func (s *S) TestNewShouldNotStoreRepoInDbWhenBareCreationFails(c *C) {
+func (s *S) TestNewShouldNotStoreRepoInDbWhenBareCreationFails(c *gocheck.C) {
 	dir, err := commandmocker.Error("git", "", 1)
-	c.Check(err, IsNil)
+	c.Check(err, gocheck.IsNil)
 	defer commandmocker.Remove(dir)
 	r, err := New("myRepo", []string{"pumpkin"}, true)
-	c.Check(err, NotNil)
+	c.Check(err, gocheck.NotNil)
 	err = db.Session.Repository().Find(bson.M{"_id": r.Name}).One(&r)
-	c.Assert(err, ErrorMatches, "^not found$")
+	c.Assert(err, gocheck.ErrorMatches, "^not found$")
 }
 
-func (s *S) TestRemoveShouldRemoveBareRepositoryFromFileSystem(c *C) {
+func (s *S) TestRemoveShouldRemoveBareRepositoryFromFileSystem(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	rfs := &fstesting.RecordingFs{FileContent: "foo"}
 	fs.Fsystem = rfs
 	defer func() { fs.Fsystem = nil }()
 	r, err := New("myRepo", []string{"pumpkin"}, false)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = Remove(r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	action := "removeall " + path.Join(bareLocation(), "myRepo.git")
-	c.Assert(rfs.HasAction(action), Equals, true)
+	c.Assert(rfs.HasAction(action), gocheck.Equals, true)
 }
 
-func (s *S) TestRemoveShouldRemoveRepositoryFromDatabase(c *C) {
+func (s *S) TestRemoveShouldRemoveRepositoryFromDatabase(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	rfs := &fstesting.RecordingFs{FileContent: "foo"}
 	fs.Fsystem = rfs
 	defer func() { fs.Fsystem = nil }()
 	r, err := New("myRepo", []string{"pumpkin"}, false)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = Remove(r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().Find(bson.M{"_id": r.Name}).One(&r)
-	c.Assert(err, ErrorMatches, "^not found$")
+	c.Assert(err, gocheck.ErrorMatches, "^not found$")
 }
 
-func (s *S) TestRemoveShouldReturnMeaningfulErrorWhenRepositoryDoesNotExistsInDatabase(c *C) {
+func (s *S) TestRemoveShouldReturnMeaningfulErrorWhenRepositoryDoesNotExistsInDatabase(c *gocheck.C) {
 	rfs := &fstesting.RecordingFs{FileContent: "foo"}
 	fs.Fsystem = rfs
 	defer func() { fs.Fsystem = nil }()
 	r := &Repository{Name: "fooBar"}
 	err := Remove(r)
-	c.Assert(err, ErrorMatches, "^Could not remove repository: not found$")
+	c.Assert(err, gocheck.ErrorMatches, "^Could not remove repository: not found$")
 }
 
-func (s *S) TestGitUrl(c *C) {
+func (s *S) TestGitUrl(c *gocheck.C) {
 	host, err := config.GetString("host")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	remote := (&Repository{Name: "lol"}).GitUrl()
-	c.Assert(remote, Equals, fmt.Sprintf("git://%s/lol.git", host))
+	c.Assert(remote, gocheck.Equals, fmt.Sprintf("git://%s/lol.git", host))
 }
 
-func (s *S) TestSshUrl(c *C) {
+func (s *S) TestSshUrl(c *gocheck.C) {
 	host, err := config.GetString("host")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	remote := (&Repository{Name: "lol"}).SshUrl()
-	c.Assert(remote, Equals, fmt.Sprintf("git@%s:lol.git", host))
+	c.Assert(remote, gocheck.Equals, fmt.Sprintf("git@%s:lol.git", host))
 }
 
-func (s *S) TestSshUrlUseUidFromConfigFile(c *C) {
+func (s *S) TestSshUrlUseUidFromConfigFile(c *gocheck.C) {
 	uid, err := config.GetString("uid")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	host, err := config.GetString("host")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	config.Set("uid", "test")
 	defer config.Set("uid", uid)
 	remote := (&Repository{Name: "f#"}).SshUrl()
-	c.Assert(remote, Equals, fmt.Sprintf("test@%s:f#.git", host))
+	c.Assert(remote, gocheck.Equals, fmt.Sprintf("test@%s:f#.git", host))
 }
 
-func (s *S) TestGrantAccessShouldAddUserToListOfRepositories(c *C) {
+func (s *S) TestGrantAccessShouldAddUserToListOfRepositories(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	r, err := New("proj1", []string{"someuser"}, true)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r.Name)
 	r2, err := New("proj2", []string{"otheruser"}, true)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r2.Name)
 	u := struct {
 		Name string `bson:"_id"`
 	}{Name: "lolcat"}
 	err = db.Session.User().Insert(&u)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.User().RemoveId(u.Name)
 	err = GrantAccess([]string{r.Name, r2.Name}, []string{u.Name})
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r.Name).One(&r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r2.Name).One(&r2)
-	c.Assert(err, IsNil)
-	c.Assert(r.Users, DeepEquals, []string{"someuser", u.Name})
-	c.Assert(r2.Users, DeepEquals, []string{"otheruser", u.Name})
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(r.Users, gocheck.DeepEquals, []string{"someuser", u.Name})
+	c.Assert(r2.Users, gocheck.DeepEquals, []string{"otheruser", u.Name})
 }
 
-func (s *S) TestGrantAccessShouldAddFirstUserIntoRepositoryDocument(c *C) {
+func (s *S) TestGrantAccessShouldAddFirstUserIntoRepositoryDocument(c *gocheck.C) {
 	r := Repository{Name: "proj1"}
 	err := db.Session.Repository().Insert(&r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r.Name)
 	r2 := Repository{Name: "proj2"}
 	err = db.Session.Repository().Insert(&r2)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r2.Name)
 	err = GrantAccess([]string{r.Name, r2.Name}, []string{"Umi"})
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r.Name).One(&r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r.Name).One(&r2)
-	c.Assert(err, IsNil)
-	c.Assert(r.Users, DeepEquals, []string{"Umi"})
-	c.Assert(r2.Users, DeepEquals, []string{"Umi"})
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(r.Users, gocheck.DeepEquals, []string{"Umi"})
+	c.Assert(r2.Users, gocheck.DeepEquals, []string{"Umi"})
 }
 
-func (s *S) TestGrantAccessShouldSkipDuplicatedUsers(c *C) {
+func (s *S) TestGrantAccessShouldSkipDuplicatedUsers(c *gocheck.C) {
 	r := Repository{Name: "proj1", Users: []string{"umi", "luke", "pade"}}
 	err := db.Session.Repository().Insert(&r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r.Name)
 	err = GrantAccess([]string{r.Name}, []string{"pade"})
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r.Name).One(&r)
-	c.Assert(err, IsNil)
-	c.Assert(r.Users, DeepEquals, []string{"umi", "luke", "pade"})
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(r.Users, gocheck.DeepEquals, []string{"umi", "luke", "pade"})
 }
 
-func (s *S) TestRevokeAccessShouldRemoveUserFromAllRepositories(c *C) {
+func (s *S) TestRevokeAccessShouldRemoveUserFromAllRepositories(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	r, err := New("proj1", []string{"someuser", "umi"}, true)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r.Name)
 	r2, err := New("proj2", []string{"otheruser", "umi"}, true)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer db.Session.Repository().RemoveId(r2.Name)
 	err = RevokeAccess([]string{r.Name, r2.Name}, []string{"umi"})
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r.Name).One(&r)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	err = db.Session.Repository().FindId(r2.Name).One(&r2)
-	c.Assert(err, IsNil)
-	c.Assert(r.Users, DeepEquals, []string{"someuser"})
-	c.Assert(r2.Users, DeepEquals, []string{"otheruser"})
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(r.Users, gocheck.DeepEquals, []string{"someuser"})
+	c.Assert(r2.Users, gocheck.DeepEquals, []string{"otheruser"})
 }
 
-func (s *S) TestConflictingRepositoryNameShouldReturnExplicitError(c *C) {
+func (s *S) TestConflictingRepositoryNameShouldReturnExplicitError(c *gocheck.C) {
 	tmpdir, err := commandmocker.Add("git", "$*")
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	defer commandmocker.Remove(tmpdir)
 	_, err = New("someRepo", []string{"gollum"}, true)
 	defer db.Session.Repository().Remove(bson.M{"_id": "someRepo"})
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	_, err = New("someRepo", []string{"gollum"}, true)
-	c.Assert(err, ErrorMatches, "A repository with this name already exists.")
+	c.Assert(err, gocheck.ErrorMatches, "A repository with this name already exists.")
 }
 
-func (s *S) TestGet(c *C) {
+func (s *S) TestGet(c *gocheck.C) {
 	repo := Repository{Name: "somerepo", Users: []string{}}
 	err := db.Session.Repository().Insert(repo)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	r, err := Get("somerepo")
-	c.Assert(err, IsNil)
-	c.Assert(r, DeepEquals, repo)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(r, gocheck.DeepEquals, repo)
 }
 
-func (s *S) TestMarshalJSON(c *C) {
+func (s *S) TestMarshalJSON(c *gocheck.C) {
 	repo := Repository{Name: "somerepo", Users: []string{}}
 	expected := map[string]interface{}{
 		"name":    repo.Name,
@@ -299,9 +299,9 @@ func (s *S) TestMarshalJSON(c *C) {
 		"git_url": repo.GitUrl(),
 	}
 	data, err := json.Marshal(&repo)
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 	var result map[string]interface{}
 	err = json.Unmarshal(data, &result)
-	c.Assert(err, IsNil)
-	c.Assert(result, DeepEquals, expected)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(result, gocheck.DeepEquals, expected)
 }
