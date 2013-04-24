41349fd3303f66bb2b3ce2777fd1951fc2e22259
Francisco Souza <fss@corp.globo.com>
2013-04-24 12:44:37 -0300

    db: don't use unqualified imports for gocheck

--- db/conn_test.go
+++ db/conn_test.go
@@ -7,58 +7,58 @@ package db
 import (
 	"github.com/globocom/config"
 	"labix.org/v2/mgo"
-	. "launchpad.net/gocheck"
+	"launchpad.net/gocheck"
 	"testing"
 )
 
-func Test(t *testing.T) { TestingT(t) }
+func Test(t *testing.T) { gocheck.TestingT(t) }
 
 type S struct{}
 
-var _ = Suite(&S{})
+var _ = gocheck.Suite(&S{})
 
-func (s *S) SetUpSuite(c *C) {
+func (s *S) SetUpSuite(c *gocheck.C) {
 	config.Set("database:url", "127.0.0.1:27017")
 	config.Set("database:name", "gandalf_tests")
 	Connect()
 }
 
-func (s *S) TearDownSuite(c *C) {
+func (s *S) TearDownSuite(c *gocheck.C) {
 	Session.DB.DropDatabase()
 }
 
-func (s *S) TestSessionRepositoryShouldReturnAMongoCollection(c *C) {
+func (s *S) TestSessionRepositoryShouldReturnAMongoCollection(c *gocheck.C) {
 	var rep *mgo.Collection
 	rep = Session.Repository()
 	cRep := Session.DB.C("repository")
-	c.Assert(rep, DeepEquals, cRep)
+	c.Assert(rep, gocheck.DeepEquals, cRep)
 }
 
-func (s *S) TestSessionUserShouldReturnAMongoCollection(c *C) {
+func (s *S) TestSessionUserShouldReturnAMongoCollection(c *gocheck.C) {
 	var usr *mgo.Collection
 	usr = Session.User()
 	cUsr := Session.DB.C("user")
-	c.Assert(usr, DeepEquals, cUsr)
+	c.Assert(usr, gocheck.DeepEquals, cUsr)
 }
 
-func (s *S) TestSessionKeyShouldReturnKeyCollection(c *C) {
+func (s *S) TestSessionKeyShouldReturnKeyCollection(c *gocheck.C) {
 	key := Session.Key()
 	cKey := Session.DB.C("key")
-	c.Assert(key, DeepEquals, cKey)
+	c.Assert(key, gocheck.DeepEquals, cKey)
 }
 
-func (s *S) TestSessionKeyBodyIsUnique(c *C) {
+func (s *S) TestSessionKeyBodyIsUnique(c *gocheck.C) {
 	key := Session.Key()
 	indexes, err := key.Indexes()
-	c.Assert(err, IsNil)
-	c.Assert(indexes, HasLen, 2)
-	c.Assert(indexes[1].Key, DeepEquals, []string{"body"})
-	c.Assert(indexes[1].Unique, DeepEquals, true)
+	c.Assert(err, gocheck.IsNil)
+	c.Assert(indexes, gocheck.HasLen, 2)
+	c.Assert(indexes[1].Key, gocheck.DeepEquals, []string{"body"})
+	c.Assert(indexes[1].Unique, gocheck.DeepEquals, true)
 }
 
-func (s *S) TestConnect(c *C) {
+func (s *S) TestConnect(c *gocheck.C) {
 	Connect()
-	c.Assert(Session.DB.Name, Equals, "gandalf_tests")
+	c.Assert(Session.DB.Name, gocheck.Equals, "gandalf_tests")
 	err := Session.DB.Session.Ping()
-	c.Assert(err, IsNil)
+	c.Assert(err, gocheck.IsNil)
 }
