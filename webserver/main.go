18bc2efa948d01f0a216bb46acc39f159589338f
Francisco Souza <fss@corp.globo.com>
2013-10-14 12:12:54 -0300

    webserver: map healthcheck handler

--- webserver/main.go
+++ webserver/main.go
@@ -38,6 +38,7 @@ For an example conf check gandalf/etc/gandalf.conf file.\n %s`
 	router.Del("/repository/:name", http.HandlerFunc(api.RemoveRepository))
 	router.Get("/repository/:name", http.HandlerFunc(api.GetRepository))
 	router.Put("/repository/:name", http.HandlerFunc(api.RenameRepository))
+	router.Get("/healthcheck/", http.HandlerFunc(api.HealthCheck))
 
 	bind, err := config.GetString("bind")
 	if err != nil {
