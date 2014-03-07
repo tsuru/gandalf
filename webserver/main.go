a1cd1313e3534bb8a661849c3ea9921bb13d1d05
Francisco Souza <fss@corp.globo.com>
2014-03-07 16:08:44 -0300

    webserver/main: fix version
    My bad :(

--- webserver/main.go
+++ webserver/main.go
@@ -1,4 +1,4 @@
-// Copyright 2013 gandalf authors. All rights reserved.
+// Copyright 2014 gandalf authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -15,7 +15,7 @@ import (
 	"net/http"
 )
 
-const version = "0.2.0"
+const version = "0.2.2"
 
 func main() {
 	dry := flag.Bool("dry", false, "dry-run: does not start the server (for testing purpose)")
