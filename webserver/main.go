fdcf825903d4d2b87a5da96efebf77cf42de332a
Marcelo Jorge Vieira <metal@alucinados.com>
2014-12-15 13:38:33 -0200

    Release 0.5.2

--- webserver/main.go
+++ webserver/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/tsuru/gandalf/api"
 )
 
-const version = "0.5.1"
+const version = "0.5.2"
 
 func main() {
 	dry := flag.Bool("dry", false, "dry-run: does not start the server (for testing purpose)")
