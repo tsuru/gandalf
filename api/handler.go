9b1501b3c743e5bd8047942c9d68f65a1ee5b92f
Marc Abramowitz <marc@marc-abramowitz.com>
2015-01-02 23:37:53 -0800

    Include more info in /healthcheck
    Before:
    
        [marca@marca-mac2 ~]$ http http://127.0.0.1:8000/healthcheck
        HTTP/1.1 200 OK
        Content-Length: 7
        Content-Type: text/plain; charset=utf-8
        Date: Sat, 03 Jan 2015 07:40:52 GMT
    
        WORKING
    
    After:
    
        [marca@marca-mac2 ~]$ http http://127.0.0.1:8000/healthcheck
        HTTP/1.1 200 OK
        Content-Length: 189
        Content-Type: application/json
        Date: Sat, 03 Jan 2015 07:39:04 GMT
    
        {
            "database": {
                "name": "gandalf",
                "url": "127.0.0.1:27017"
            },
            "git": {
                "bare_location": "/var/lib/gandalf/repositories",
                "bare_template": "/home/git/bare-template"
            },
            "pid": 75611,
            "status": "OK",
            "uid": 502
        }

--- api/handler.go
+++ api/handler.go
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 	"mime"
 	"net/http"
+	"os"
 	"path/filepath"
 	"reflect"
 	"strconv"
@@ -308,6 +309,16 @@ func parseBody(body io.ReadCloser, result interface{}) error {
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request) {
+	dburl, _ := config.GetString("database:url")
+	if dburl == "" {
+		dburl = db.DefaultDatabaseURL
+	}
+	dbname, _ := config.GetString("database:name")
+	if dbname == "" {
+		dbname = db.DefaultDatabaseName
+	}
+	bareLocation, _ := config.GetString("git:bare:location")
+	bareTemplate, _ := config.GetString("git:bare:template")
 	conn, err := db.Conn()
 	if err != nil {
 		return
@@ -318,7 +329,21 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Failed to ping the database: %s\n", err)
 		return
 	}
-	w.Write([]byte("WORKING"))
+	result := make(map[string]interface{})
+	database := make(map[string]interface{})
+	database["url"] = dburl
+	database["name"] = dbname
+	git := make(map[string]interface{})
+	git["bare_location"] = bareLocation
+	git["bare_template"] = bareTemplate
+	result["database"] = database
+	result["git"] = git
+	result["pid"] = os.Getpid()
+	result["uid"] = os.Getuid()
+	result["status"] = "OK"
+	w.Header().Set("Content-Type", "application/json")
+	b, err := json.Marshal(result)
+	w.Write(b)
 }
 
 func getMimeType(path string, content []byte) string {
