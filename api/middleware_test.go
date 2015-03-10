877cd2b9377f1a4402ebc519595e12bb472d4586
Francisco Souza <fss@corp.globo.com>
2015-03-10 11:35:25 -0300

    api: add tests for the responseHeaderMiddleware
    The test that ensures that handlers can override the default header is
    pretty important.

--- api/middleware_test.go
+++ api/middleware_test.go
@@ -30,3 +30,26 @@ func (s *S) TestLoggerMiddleware(c *check.C) {
 	timePart := time.Now().Format(time.RFC3339Nano)[:19]
 	c.Assert(out.String(), check.Matches, fmt.Sprintf(`(?m)%s\..+? PUT /my/path 200 in [\d.]+ms$`, timePart))
 }
+
+func (s *S) TestResponseHeaderMiddleware(c *check.C) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("PUT", "/my/path", nil)
+	c.Assert(err, check.IsNil)
+	middle := responseHeaderMiddleware{name: "Server", value: "super-server/0.1"}
+	middle.ServeHTTP(negroni.NewResponseWriter(recorder), request, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	}))
+	c.Assert(recorder.Header().Get("Server"), check.Equals, "super-server/0.1")
+}
+
+func (s *S) TestResponseHeaderMiddlewareDuplicate(c *check.C) {
+	recorder := httptest.NewRecorder()
+	request, err := http.NewRequest("PUT", "/my/path", nil)
+	c.Assert(err, check.IsNil)
+	middle := responseHeaderMiddleware{name: "Server", value: "super-server/0.1"}
+	middle.ServeHTTP(negroni.NewResponseWriter(recorder), request, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Server", "mini-server/0.1")
+		w.Write([]byte("hello"))
+	}))
+	c.Assert(recorder.Header().Get("Server"), check.Equals, "mini-server/0.1")
+}
