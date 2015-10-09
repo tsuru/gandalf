62c82eb4151d86b4cba86cbd720af0492d76f84b
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-10-09 16:39:43 -0300

    webserver: bump to 0.7.3

--- webserver/main.go
+++ webserver/main.go
@@ -15,7 +15,7 @@ import (
 	"github.com/tsuru/tsuru/log"
 )
 
-const version = "0.7.2"
+const version = "0.7.3"
 
 func main() {
 	dry := flag.Bool("dry", false, "dry-run: does not start the server (for testing purpose)")
