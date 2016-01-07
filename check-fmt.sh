47f5c212aadf1f418b84199a6839be3ac2acade1
Francisco Souza <fss@corp.globo.com>
2016-01-07 16:48:46 -0200

    check-fmt: ignore gofmt failures on vendored dependencies
    Also disabling goimports for now. See golang/go#13644.

--- check-fmt.sh
+++ check-fmt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-# Copyright 2014 tsuru authors. All rights reserved.
+# Copyright 2016 tsuru authors. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 
@@ -8,7 +8,7 @@
 # to "tsuru authors" instead of "gandalf authors".
 
 status=0
-out=`gofmt -s -l .`
+out=`gofmt -s -l . | grep -v vendor/`
 if [ "${out}" != "" ]
 then
     echo "ERROR: there are files that need to be formatted with gofmt"
@@ -22,19 +22,22 @@ then
     status=1
 fi
 
-go get golang.org/x/tools/cmd/goimports
-out=`goimports -l .`
-if [ "${out}" != "" ]
-then
-    echo "ERROR: there are files that need to be formatted with goimports"
-    echo
-    echo "Files:"
-    for file in $out
-    do
-        echo "- ${file}"
-    done
-    status=1
-fi
+# TODO(fss): uncomment this code when golang/go#13644 is fixed
+#            (https://github.com/golang/go/issues/13644).
+#
+# go get golang.org/x/tools/cmd/goimports
+# out=`goimports -l .`
+# if [ "${out}" != "" ]
+# then
+#     echo "ERROR: there are files that need to be formatted with goimports"
+#     echo
+#     echo "Files:"
+#     for file in $out
+#     do
+#         echo "- ${file}"
+#     done
+#     status=1
+# fi
 
 
 go get golang.org/x/tools/cmd/vet
